1d4ab48ec06e3e21ef89d8e6f57586a5f9246d19
Sophi Weiss <Owlety34@gmail.com>
2023-01-31 22:30:48 +0100

    adding function in js file

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-let myNodelist = document.getElementsByTagName("list");
-let i;
-for (i = 0; i < myNodelist.length; i++) {
-  let button = document.createElement('button');
-  let text = document.createTextNode('\u00d7');
-  button.className = 'close';
-  button.appendChild(text)
-  myNodelist[i].appendChild(button);
+document.querySelector('#button-push').onclick = function() {
+  if (document.querySelector('#textInput').value.length === 0) {
+    alert("Please enter at least something🤍")
+  }
 }
-
-
