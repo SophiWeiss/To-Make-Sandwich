c2197420d38b5ae99f7fb53c44d097d06367aad6
Sophi Weiss <Owlety34@gmail.com>
2023-01-31 22:36:10 +0100

    adding function in js file

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.querySelector('#button-push').onclick = function() {
-  if (document.querySelector('#textInput').value.length === 0) {
+  if (document.querySelector('#textInput').value.length === 0 || ' ') {
     alert("Please enter at least something🤍")
   }
 }
