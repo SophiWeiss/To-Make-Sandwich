83111a2611f11b1dad107919d05cf5f2cbf703bc
Sophi Weiss <Owlety34@gmail.com>
2023-02-02 14:36:34 +0100

    Buttons Edit (Edit, Save and Cancel) and Delete styled and some parts of list can be checked as "done"

--- main.js
+++ main.js
@@ -11,80 +11,109 @@ function checkInput () {
 
     let ul = document.getElementById("ulOfTasks");
     let li = document.createElement("li");
-    
-    //current time 
+
+    //current time
     let currentTime = new Date().toLocaleString();
     let currentTimeSpan = document.createElement("span");
     currentTimeSpan.style.fontSize = "11px";
     currentTimeSpan.style.color = "grey";
     currentTimeSpan.appendChild(document.createTextNode(" (" + currentTime + ")"));
-    
+
     li.appendChild(document.createTextNode(inputValue));
     li.appendChild(currentTimeSpan);
+
     
     //edit button
     let editButton = document.createElement("button");
     editButton.appendChild(document.createTextNode("Edit"));
-    
+
     editButton.addEventListener("click", function() {
+      let originalText = li.firstChild.nodeValue;
       let input = document.createElement("input");
-      input.value = li.firstChild.nodeValue;
-      // li.removeChild(li.firstChild);
-      // li.insertBefore(input, li.firstChild);
+      input.value = originalText;
       li.replaceChild(input, li.firstChild);
       editButton.remove()
       deleteButton.remove()
       currentTimeSpan.remove()
-      
+
       let saveButton = document.createElement("button");
       saveButton.appendChild(document.createTextNode("Save"));
       saveButton.addEventListener("click", function() {
         let text = document.createTextNode(input.value);
         li.removeChild(input);
         li.insertBefore(text, li.firstChild);
-        li.appendChild(editButton)
         li.appendChild(deleteButton)
+        li.appendChild(editButton)
         li.appendChild(currentTimeSpan)
-        
+
         saveButton.remove()
         cancelButton.remove()
       });
       li.appendChild(saveButton);
 
+      //style settings for saveButton
+      saveButton.style.backgroundColor = "#d074d7";
+      saveButton.style.color = "white";
+      saveButton.style.padding = "5px 7px";
+      saveButton.style.borderRadius = "5px";
+      saveButton.style.cursor = "pointer";
+      saveButton.style.border = 'none'
+
       let cancelButton = document.createElement("button");
       cancelButton.appendChild(document.createTextNode("Cancel"));
       cancelButton.addEventListener("click", function() {
-        let text = document.createTextNode(input.value);
         li.removeChild(input);
-        li.insertBefore(text, li.firstChild);
-        li.appendChild(editButton)
+        li.insertBefore(document.createTextNode(originalText), li.firstChild);
         li.appendChild(deleteButton)
+        li.appendChild(editButton)
         li.appendChild(currentTimeSpan)
-        
         saveButton.remove()
         cancelButton.remove()
-
-        input.value = 
       });
       li.appendChild(cancelButton);
+      
+      //style settings for cancelButton
+      cancelButton.style.backgroundColor = "#7e74d7";
+      cancelButton.style.color = "white";
+      cancelButton.style.padding = "5px 7px";
+      cancelButton.style.borderRadius = "5px";
+      cancelButton.style.cursor = "pointer";
+      cancelButton.style.border = 'none'
     });
-    
-    li.appendChild(editButton);
 
-    //delete button 
+    //delete button
     let deleteButton = document.createElement("button");
     deleteButton.appendChild(document.createTextNode("Delete"));
-    
+
     deleteButton.addEventListener("click", function() {
       ul.removeChild(li);
     });
-    
-    li.appendChild(deleteButton);
 
-    
+    //appending two main buttons
+    li.appendChild(deleteButton);
+    li.appendChild(editButton);
+
+
     ul.appendChild(li);
 
     document.getElementById("textInput").value = "";
+
+    //style settings for buttons
+    editButton.style.backgroundColor = "#d074d7";
+    editButton.style.color = "white";
+    editButton.style.padding = "5px 7px";
+    editButton.style.borderRadius = "5px";
+    editButton.style.cursor = "pointer";
+    editButton.style.border = 'none'
+    editButton.style.float = "right";
+
+    deleteButton.style.backgroundColor = "#7e74d7";
+    deleteButton.style.color = "white";
+    deleteButton.style.padding = "5px 7px";
+    deleteButton.style.borderRadius = "5px";
+    deleteButton.style.cursor = "pointer";
+    deleteButton.style.border = 'none'
+    deleteButton.style.float = "right";
     
   }
   
@@ -95,3 +124,14 @@ document.getElementById("textInput").addEventListener("keydown", function(event)
     document.getElementById("button-push").click();
   }
 });
+
+let lis = document.querySelectorAll("li");
+for (let i = 0; i < lis.length; i++) {
+  lis[i].addEventListener("click", function() {
+    if (this.style.textDecoration === "line-through") {
+      this.style.textDecoration = "none";
+    } else {
+      this.style.textDecoration = "line-through";
+    }
+  });
+}
