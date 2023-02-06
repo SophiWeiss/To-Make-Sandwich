0a0390ed38b1efd56047e34aff5287288eb0b3f9
Sophi Weiss <Owlety34@gmail.com>
2023-02-07 00:22:13 +0100

    Changes in js file, trying to rewrite everything in shorter(functional) way

--- storing.js
+++ storing.js
@@ -82,12 +82,55 @@ function addTodo(value) {
   todoList.appendChild(todoItem);
 }
 
-function editTodo() {
-  // code for editing a todo item
+function editTodo(li) {
+  let originalText = li.firstChild.firstChild.nodeValue;
+  let input = document.createElement("input");
+  input.value = originalText;
+  li.replaceChild(input, li.firstChild);
+  let deleteButton = li.getElementsByClassName("deleteButton")[0];
+  let editButton = li.getElementsByClassName("editButton")[0];
+  let currentTimeSpan = li.getElementsByTagName("span")[1];
+  deleteButton.remove();
+  editButton.remove();
+  currentTimeSpan.remove();
+
+  let cancelButton = document.createElement("button");
+  cancelButton.appendChild(document.createTextNode("Cancel"));
+  cancelButton.addEventListener("click", function() {
+    li.replaceChild(span, input);
+    li.appendChild(deleteButton);
+    li.appendChild(editButton);
+    li.appendChild(currentTimeSpan);
+    saveButton.remove();
+    cancelButton.remove();
+    li.click();
+    li.click();
+    li.click();
+  });
+  li.appendChild(cancelButton);
+
+  let saveButton = document.createElement("button");
+  saveButton.appendChild(document.createTextNode("Save"));
+  saveButton.addEventListener("click", function() {
+    let span = li.firstChild;
+    span.textContent = input.value;
+    li.removeChild(input);
+    li.insertBefore(span, li.firstChild);
+    li.appendChild(deleteButton);
+    li.appendChild(editButton);
+    li.appendChild(currentTimeSpan);
+    saveButton.remove();
+    cancelButton.remove();
+    li.click();
+    li.click();
+    li.click();
+  });
+  li.appendChild(saveButton);
 }
 
-function deleteTodo() {
-  // code for deleting a todo item
+function deleteTodo(li) {
+  let ul = document.getElementById("ulOfTasks");
+  ul.removeChild(li);
 }
 
 function checkInput() {
