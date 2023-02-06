5b8c645e2fdbd7f6140b10e9a2462c28e30d9c83
Sophi Weiss <Owlety34@gmail.com>
2023-02-07 00:36:34 +0100

    Changes in js file, trying to rewrite everything in shorter(functional) way

--- storing.js
+++ storing.js
@@ -1,26 +1,3 @@
-// // Load to-do items from local storage
-// const loadTodos = () => {
-//   const todos = JSON.parse(localStorage.getItem("todos")) || [];
-//
-//   todos.forEach(todo => {
-//     const li = document.createElement("li");
-//     li.innerHTML = `
-//       ${todo.text}
-//       <small>${todo.currentTimeSpan}</small>
-//       <button id="editButton" >Edit</button>
-//       <button id="deleteButton" >Delete</button>
-//     `;
-//     todoList.appendChild(li);
-//   });
-// };
-//
-// // Save to-do items to local storage
-// const saveTodos = todos => {
-//   localStorage.setItem("todos", JSON.stringify(todos));
-// };
-//
-// loadTodos();
-
 function getInputValue() {
   return document.getElementById("textInput").value;
 }
